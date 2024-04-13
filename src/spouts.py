29751e8d366f7f94752831d4a425e0763944c473
HP <vanhao.truong@gmail.com>
2024-04-13 10:17:21 +0700

    change the csv_file_path

--- src/spouts.py
+++ src/spouts.py
@@ -7,7 +7,7 @@ class Wind_Spout(Spout):
 
     def initialize(self, stormconf, context):
 
-        csv_file_path = '/home/hp/Study/01_Ky_thuat_du_lieu/bai_tap_lon/raw_data/raw_wind_sensor.csv'
+        csv_file_path = '/home/hp/Study/01_Ky_thuat_du_lieu/cassandra_apache_storm/raw_data/raw_wind_sensor.csv'
         self.file = open(csv_file_path, 'r')
         self.reader = csv.reader(self.file)
         next(self.reader, None)  # Skip the header
@@ -30,7 +30,7 @@ class Weather_Spout(Spout):
 
     def initialize(self, stormconf, context):
         # Open the CSV file
-        csv_file_path ='/home/hp/Study/01_Ky_thuat_du_lieu/bai_tap_lon/raw_data/raw_weather_sensor.csv'
+        csv_file_path ='/home/hp/Study/01_Ky_thuat_du_lieu/cassandra_apache_storm/raw_data/raw_weather_sensor.csv'
         self.file = open(csv_file_path, 'r')
         self.reader = csv.reader(self.file)
         next(self.reader, None)  # Skip the header

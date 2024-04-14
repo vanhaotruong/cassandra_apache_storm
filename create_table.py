dc406f3a681677b5eb703ffaa32b28b9493c386a
HP <vanhao.truong@gmail.com>
2024-04-14 16:44:34 +0700

    Modified to be session = cluster.connect('iotsolution')

--- create_table.py
+++ create_table.py
@@ -2,7 +2,7 @@ from cassandra.cluster import Cluster
 
 if __name__ == "__main__":
     cluster = Cluster(['127.0.0.1'])
-    session = cluster.connect()
+    session = cluster.connect('iotsolution')
 
     # Read the CQL commands from the de_project.cql file
     with open('de_project.cql', 'r') as file:
